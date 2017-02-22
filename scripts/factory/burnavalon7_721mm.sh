99477a6c0c0af60dd62236d8326458a28b193d68
jianxiong <jianxiong@canaan-creative.com>
2017-02-22 15:10:16 +0800

    add argument in burn script

--- scripts/factory/burnavalon7_721mm.sh
+++ scripts/factory/burnavalon7_721mm.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd /home/Burn_tool_script
 while true; do 
-        make isedir=/home/factory/Xilinx/14.6/ISE_DS reflash;
+        make isedir=/home/factory/Xilinx/14.6/ISE_DS reflash HARDWARE_NAME="MM721";
 echo;
 read -p "Press any key to burn next"
 done
--- scripts/factory/burnavalon7_721pmu.sh
+++ scripts/factory/burnavalon7_721pmu.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd /home/factory/Burn_tool_script
 while true; do
-	make reflash_ulink2
+	make reflash_ulink2 pmu="pmu721"
 echo;
 read -p "Press any key to burn next"
 done
