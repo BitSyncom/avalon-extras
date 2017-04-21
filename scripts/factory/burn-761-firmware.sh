bb119590c039be3829691a790421d1fe88cea575
jianxiong <jianxiong@canaan-creative.com>
2017-04-21 16:17:49 +0800

    Delete burn-761-firmware.sh

--- scripts/factory/burn-761-firmware.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-cd /home/factory/Avalon-extras/scripts/factory
-while true; do
-        makei isedir=/home/factory/Xilinx/14.6/ISE_DS reflash PLATFORM=MM761;
-	make reflash_ulink2 PMU=mcu
-echo;
-read -p "Press any key to burn next"
-done
