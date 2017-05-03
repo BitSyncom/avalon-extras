ab53681b4031f6f7c392cfbce7978601c2e54f0d
jianxiong <jianxiong@canaan-creative.com>
2017-05-03 15:05:21 +0800

    Add burn 761 firmware scripts

--- scripts/factory/burn-mm.sh
+++ scripts/factory/burn-mm.sh
@@ -3,5 +3,8 @@ cd /home/factory/Avalon-extras/scripts/factory
 while true; do
         make isedir=/home/factory/Xilinx/14.6/ISE_DS reflash PLATFORM=$1
 echo;
+if [ "$2" != "" ];then
+break	
+fi
 read -p "Press any key to burn next"
 done
